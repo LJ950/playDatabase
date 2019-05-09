77702ae20d60c993ad742bd1d4ae2d2acfddff7b
balshergill <baldipkshergill@gmail.com>
2019-05-09 10:20:52 +0100

    database created and data seeded

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,10 @@
-const knex = require("knex");
-const dbConfig = require("../knexfile");
+//const knex = require("knex");
+const dbConfig =
+  process.env.NODE_ENV === "production"
+    ? { client: "pg", connection: `${process.env.DATABASE_URL}?ssl=true` }
+    : require("../knexfile");
 
-const connection = knex(dbConfig);
+//const connection = knex(dbConfig);
 
-module.exports = connection;
+module.exports = require("knex")(dbConfig);
+//module.exports = connection;
